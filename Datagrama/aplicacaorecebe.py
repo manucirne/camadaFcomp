b5168aa1ef85d251af47fac1a2ae237c1031fac3
manucirne <manucirne@gmail.com>
2018-08-23 11:49:23 -0300

    resolvendo velocidades e iniciando head e EOP

---
+++ Datagrama/aplicacaorecebe.py
@@ -0,0 +1,74 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Aplicação
+####################################################
+
+print("comecou")
+
+from enlace import *
+import time
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer a
+# comunicaçao
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM5"                  # Windows(variacao de)
+
+
+
+print("porta COM aberta com sucesso")
+
+
+
+def main():
+    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+    com = enlace(serialName)
+
+    # Ativa comunicacao
+    com.enable()
+
+    #verificar que a comunicação foi aberta
+    print("comunicação aberta")
+
+
+  
+    # Atualiza dados da transmissão
+    txSize = com.tx.getStatus()
+   
+
+    # Faz a recepção dos dados
+    print ("Recebendo dados .... ")
+    bytesSeremLidos=com.rx.getBufferLen()
+  
+        
+    rxBuffer, nRx = com.getData()
+
+    with open("recebida", "wb+") as imageFile:
+        imagemrecebida = imageFile.write(rxBuffer)
+
+    # log
+    print ("Lido              {} bytes ".format(nRx))
+    
+    print (rxBuffer)
+
+    
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
