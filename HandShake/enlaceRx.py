eca1cc71c2129fc4afe6288ca8617d805f94a2ec
manucirne <manucirne@gmail.com>
2018-08-31 17:09:12 -0300

    recebimento montado, sem teste

--- HandShake/enlaceRx.py
+++ HandShake/enlaceRx.py
@@ -109,10 +109,11 @@ class RX(object):
         
         #if self.getBufferLen() < size:
             #print("ERROS!!! TERIA DE LER %s E LEU APENAS %s", (size,temPraLer))
+
         while(self.getBufferLen() < size):
             time.sleep(0.05)
 #                 
-        return(self.desempacotamento()[1])
+        return(self.desempacotamento()[1], self.desempacotamento()[5])
 
 
     def clearBuffer(self):
@@ -123,26 +124,44 @@ class RX(object):
     def desempacotamento(self):
         global end, stuffing, EOP_encontrado
         cont_s = 0
+        tipo = self.buffer[5]
         tamanho_esperado = int.from_bytes(self.buffer[6:8], byteorder="big")
-        for i in range(8, len(self.buffer)-1): 
+        if tipo == bytes([4]):
+     
+            for i in range(8, len(self.buffer)-1): 
 
-            if bytes(self.buffer[i+1:i+6]) == end:
+                if bytes(self.buffer[i+1:i+6]) == end:
 
-                if  bytes([self.buffer[i-1]]) == stuffing and bytes([self.buffer[i+6]]) == stuffing:
-                    cont_s += 2
-                    zero1 = i
-                    zero2 = i+6
-                    self.buffer = self.buffer[:zero1] + self.buffer[zero1+1:zero2] + self.buffer[zero2+1:]
+                    if  bytes([self.buffer[i-1]]) == stuffing and bytes([self.buffer[i+6]]) == stuffing:
+                        cont_s += 2
+                        zero1 = i
+                        zero2 = i+6
+                        self.buffer = self.buffer[:zero1] + self.buffer[zero1+1:zero2] + self.buffer[zero2+1:]
 
-                else:
-                    tamanho_recebido = i-7+cont_s
-                    print("Tamanho Informado no Head:    ", tamanho_esperado)
-                    print("Tamanho da mensagem recebida: ", tamanho_recebido)
-                    inicioEOP = i
-                    print("Posição de início do EOP:     ",inicioEOP)
-                    print("Encontramos o fim!! :)")
-                    EOP_encontrado = True
-        return EOP_encontrado, self.buffer, inicioEOP, tamanho_esperado, tamanho_recebido
+                    else:
+                        tamanho_recebido = i-7+cont_s
+                        print("Tamanho Informado no Head:    ", tamanho_esperado)
+                        print("Tamanho da mensagem recebida: ", tamanho_recebido)
+                        inicioEOP = i
+                        print("Posição de início do EOP:     ",inicioEOP)
+                        print("Encontramos o fim!! :)")
+                        EOP_encontrado = True
+                        if tamanho_recebido == tamanho_recebido:
+                            tipo = 5
+                        else:
+                            tipo = 6
+            
+        else:
+            if tipo == 1:
+                EOP_encontrado = False
+                inicioEOP = "null"
+                tamanho_recebido = 1
+                tipo = 2
+
+        erros(tamanho_esperado,tamanho_recebido,EOP_encontrado)
+        return EOP_encontrado, self.buffer, inicioEOP, tamanho_esperado, tamanho_recebido, tipo
+            
+
 
     def erros(tamanho_esperado,tamanho_recebido):
         global EOP_encontrado
