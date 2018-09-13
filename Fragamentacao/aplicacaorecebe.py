134a41bca1dec7e601e30c3d10387975645511f4
manucirne <manucirne@gmail.com>
2018-09-13 10:39:11 -0300

    funfandinho

---
+++ Fragamentacao/aplicacaorecebe.py
@@ -0,0 +1,154 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Aplicação
+####################################################
+
+print("comecou")
+
+from enlace import *
+from enlaceRx import *
+from enlaceTx import *
+import time
+import binascii
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer a
+# comunicaçao
+# Serial Com Port
+#   para saber a sua porta, execute no terminal:
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1421"   # Mac    (variacao de)
+serialName = "COM14"                   # Windows(variacao de)
+
+
+
+print("porta COM aberta com sucesso")
+
+
+
+def main():
+    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+    com = enlace(serialName)
+
+    # Ativa comunicacao
+    com.enable()
+
+    #verificar que a comunicação foi aberta
+    print("comunicação aberta")
+
+  
+    # Atualiza dados da transmissão
+    txSize = com.tx.getStatus()
+   
+
+    # Faz a recepção dos dados
+    print ("Recebendo dados .... ")
+    bytesSeremLidos=com.rx.getBufferLen()
+  
+    tipo = 0
+    npacoteesperado = 1
+    rxBufferDF = b''
+
+    while tipo == 0:   
+        rxBuffer, nRx, erro = com.getData()
+        if len(rxBuffer) > 0:
+            tipo = rxBuffer[5]
+
+    print(". . . . . . . . . . . . . . . . . . . . . . . . . . . ")
+    print("rxBuffer: ", rxBuffer)
+    print(". . . . . . . . . . . . . . . . . . . . . . . . . . . ")
+
+
+
+    end = bytes([1,2,3,4,5])
+    stuffing = bytes(1)
+    print("Tipo:             ", tipo)
+    
+    while tipo != 7:
+        print("entrou no while")
+        if tipo == 1:
+            tipo = 2
+            print("Tipo (2):             ", tipo)
+            txBuffer0, npacote = empacotamento(bytes(1), 1, end, stuffing, tipo,1)
+            while tipo != 3:
+                com.sendData(txBuffer0)
+                rxBuffer, nRx, erro = com.getData()
+                if len(rxBuffer) > 0:
+                    tipo = rxBuffer[5]
+                if erro:
+                    tipo = 1
+                
+            print("Tipo (3):             ", tipo)
+        if tipo == 3:
+            while tipo != 4:
+                rxBuffer, nRx, erro = com.getData()
+                if len(rxBuffer) > 0:
+                    tipo = rxBuffer[5]
+                
+            #print("Tipo (4):             ", tipo)
+        if tipo == 4:
+            #print("Tipo (4):             ", tipo)
+            print("Pacote esperado:       ", npacoteesperado)
+            rxBufferD, inicioEOP, tipo, npacote, pacoteatual = desempacotamento(rxBuffer, end, stuffing, npacoteesperado)
+            
+            #print("lenBuffer", len(rxBuffer))
+            
+            print("Tipo (5,6, 7 ou 8):             ", tipo)
+            if tipo == 5:
+                txBuffer, npacotef = empacotamento(bytes(1), 1, end, stuffing, tipo,pacoteatual)
+                com.sendData(txBuffer)  
+                if (pacoteatual <= npacote):
+                    tipo = 3
+                    npacoteesperado += 1
+                    rxBufferDF += rxBufferD[8:inicioEOP+1]
+                    if pacoteatual == npacote:
+                        with open("recebida.png", "wb+") as imageFile:
+                            imagemrecebida = imageFile.write(rxBufferDF)
+                            print("rxBuffer.     ", rxBufferDF)
+                        print("Len final colado:      ", len(rxBufferDF))
+                        tipo = 7   
+                                     
+        if tipo == 6:
+            print("Erro no recebimento - 6            " , tipo)
+            txBuffer, npacote = empacotamento(bytes(1), 1, end, stuffing, tipo, 1)
+            tipo = 3
+            com.sendData(txBuffer)
+        if tipo == 8:
+            print("Erro no recebimento do pacote - 8            " , tipo)
+            txBuffer, npacote = empacotamento(bytes(1), 1, end, stuffing, tipo, npacoteesperado)
+            tipo = 3
+            com.sendData(txBuffer)
+
+    print("SAIU DO LOOP          ", tipo, "###############")
+
+    
+    
+    print("Mensagem recebida corretamente             ", tipo)
+    txBuffer,npacote = empacotamento(bytes(1), 1, end, stuffing, tipo,1)
+    com.sendData(txBuffer)
+
+
+    # log
+    print ("Lido              {} bytes ".format(rxBuffer[8:inicioEOP+1])) 
+    print(". . . . . . . . . . . . . . . . . . . . . . . . . . . ")
+    print ("rxBuffer apos retirada: ", rxBuffer)
+    print(". . . . . . . . . . . . . . . . . . . . . . . . . . . ")
+
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
---
+++ Fragamentacao/enlaceRx.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Camada de Enlace
+####################################################
+
+# Importa pacote de tempo
+import time
+
+# Threads
+import threading
+
+# Class
+class RX(object):
+    """ This class implements methods to handle the reception
+        data over the p2p fox protocol
+    """
+    
+    def __init__(self, fisica):
+        """ Initializes the TX class
+        """
+        self.fisica      = fisica
+        self.buffer      = bytes(bytearray())
+        self.threadStop  = False
+        self.threadMutex = True
+        self.READLEN     = 1024
+
+    def thread(self): 
+        """ RX thread, to send data in parallel with the code
+        essa é a funcao executada quando o thread é chamado. 
+        """
+        while not self.threadStop:
+            if(self.threadMutex == True):
+                rxTemp, nRx = self.fisica.read(self.READLEN)
+                if (nRx > 0):
+                    self.buffer += rxTemp
+                    # if nRx < int.from_bytes(self.buffer[6:8], byteorder="big"):
+                    #     self.buffer += rxTemp
+                    # else:
+                    #     self.buffer = rxTemp
+                time.sleep(0.01)
+
+    def threadStart(self):
+        """ Starts RX thread (generate and run)
+        """
+        self.thread = threading.Thread(target=self.thread, args=())
+        self.thread.start()
+
+    def threadKill(self):
+        """ Kill RX thread
+        """
+        self.threadStop = True
+
+    def threadPause(self):
+        """ Stops the RX thread to run
+
+        This must be used when manipulating the Rx buffer
+        """
+        self.threadMutex = False
+
+    def threadResume(self):
+        """ Resume the RX thread (after suspended)
+        """
+        self.threadMutex = True
+
+    def getIsEmpty(self):
+        """ Return if the reception buffer is empty
+        """
+        if(self.getBufferLen() == 0):
+            return(True)
+        else:
+            return(False)
+
+    def getBufferLen(self):
+        """ Return the total number of bytes in the reception buffer
+        """
+        return(len(self.buffer))
+
+    def getAllBuffer(self, len):
+        """ Read ALL reception buffer and clears it
+        """
+        self.threadPause()
+        b = self.buffer[:]
+        self.clearBuffer()
+        self.threadResume()
+        return(b)
+
+    def getBuffer(self, nData):
+        """ Remove n data from buffer
+        """
+        self.threadPause()
+        b           = self.buffer[0:nData]
+        self.buffer = self.buffer[nData:]
+        self.threadResume()
+        return(b)
+
+    def getNData(self):#, size):
+        """ Read N bytes of data from the reception buffer
+
+        This function blocks until the number of bytes is received
+        """
+#        temPraLer = self.getBufferLen()
+#        print('leu %s ' + str(temPraLer) )
+        
+        #if self.getBufferLen() < size:
+            #print("ERROS!!! TERIA DE LER %s E LEU APENAS %s", (size,temPraLer))
+#         while(self.getBufferLen() < size):
+#             time.sleep(0.05)
+# #                 
+#         return(self.getBuffer(size))
+        x = self.getBufferLen()
+        time.sleep(1)
+        
+
+        tInicial = time.time()
+        tFinal = time.time()
+
+        erro = False
+        #if self.getBufferLen() < size:
+            #print("ERROS!!! TERIA DE LER %s E LEU APENAS %s", (size,temPraLer))
+        while(((self.getBufferLen() == 0) or (self.getBufferLen() != x)) and tFinal - tInicial < 5 ):
+            #print("Buffer:         ", self.buffer)
+            tFinal = time.time()
+            time.sleep(1)
+            #print("Buffer:         ", self.buffer)
+            #print("lenbuffer:   ",x)
+            x = self.getBufferLen()
+            time.sleep(1)
+
+        if tFinal - tInicial >= 5 :
+            print("Erro de tempo excedido")
+            erro = True
+        x = self.getBufferLen()
+        return(self.getBuffer(x), erro)
+
+
+    def clearBuffer(self):
+        """ Clear the reception buffer
+        """
+        self.buffer = b""
+
+def desempacotamento(rxBuffer, end, stuffing, npacoteesperado):
+    print("ENTROU NO DESEMPACOTAMENTO###########################")
+    tamanho_esperado = int.from_bytes(rxBuffer[6:8], byteorder="big")
+    #print("rxBuffer:    ", rxBuffer)
+    EOP_encontrado = False
+    npacote = rxBuffer[4]
+    print("Npacote:           ", npacote)
+    pacoteatual = rxBuffer[3]
+    print("pacote atual:          ", pacoteatual)
+    cont_s = 0
+    tipo = 1
+    head = rxBuffer[:8]
+    inicioEOP = 0
+
+
+    for i in range(8, len(rxBuffer)): 
+
+        if bytes(rxBuffer[i+1:i+6]) == end:
+
+            if  bytes([rxBuffer[i-1]]) == stuffing and bytes([rxBuffer[i+6]]) == stuffing:
+                cont_s += 2
+                zero1 = i
+                zero2 = i+7
+                rxBuffer = rxBuffer[:zero1] + rxBuffer[zero1+1:zero2] + rxBuffer[zero2+1:]
+
+            else:
+                tamanho_recebido = i-7+cont_s
+                print("Tamanho Informado no Head:    ", tamanho_esperado)
+                print("Tamanho da mensagem recebida: ", tamanho_recebido)
+                inicioEOP = i
+                print("Posição de início do EOP:     ", inicioEOP)
+                print("Encontramos o fim!! :)")
+                EOP_encontrado = True
+                rxBuffer = rxBuffer[:i+5]
+                #if tamanho_esperado > 1:
+                tipo = 5
+
+
+
+    if npacoteesperado != pacoteatual:
+        print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+        print("ERRO!! Pacote esperado diferente de pacote recebido.")
+        print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+        tipo = 8
+
+    elif not EOP_encontrado:
+        print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+        print("ERRO!! O EOP não foi localizado.")
+        print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+        tipo = 6
+
+    elif tamanho_esperado != tamanho_recebido:
+        print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+        print("ERRO!! Número de bytes no payload não corresponde ao informado no head.")
+        print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+        tipo = 6
+
+    else:
+        tipo = 5
+
+    # tamanhoEmByte = (txLen).to_bytes(2,byteorder='big')
+    # vazio = bytes(5)
+    # tipo = bytes([tipo])
+    # head = vazio + tipo + tamanhoEmByte
+
+    # rxBuffer = head + rxBuffer[8:]
+
+    return rxBuffer, inicioEOP, tipo, npacote, pacoteatual
+
